52830520891794bc7f20b6714fd1382b1e2b78ab
Akshay Kumar <akshay.kesarwan@MT-C02L56HVDR53.local>
2015-11-04 16:46:47 +0530

    First cut

---
+++ src/main/java/com/tasks/manager/entities/Actor.java
@@ -0,0 +1,9 @@
+package com.tasks.manager.entities;
+
+/**
+ * Created by akshay.kesarwan on 27/10/15.
+ */
+public class Actor {
+    private String type;
+    private String id;
+}
---
+++ src/main/java/com/tasks/manager/entities/Task.java
@@ -0,0 +1,10 @@
+package com.tasks.manager.entities;
+
+/**
+ * Created by akshay.kesarwan on 27/10/15.
+ */
+public class Task {
+    private String state;
+    private String id;
+    private Actor actor;
+}
