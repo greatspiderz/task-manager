1e0f1f488cfbb7fca0de54e0a123793d2c48b151
Divya Rai <divya.rai@flipkart.com>
2015-12-02 20:25:10 +0530

    fixed NPE while add relation to task

--- src/main/java/com/tasks/manager/util/TaskManagerUtility.java
+++ src/main/java/com/tasks/manager/util/TaskManagerUtility.java
@@ -4,6 +4,8 @@ import com.tasks.manager.db.model.entities.Relation;
 import com.tasks.manager.db.model.entities.Task;
 import com.tasks.manager.db.model.entities.TaskGroup;
 
+import java.util.ArrayList;
+
 /**
  * Created by shlok.chaurasia on 19/11/15.
  */
@@ -14,7 +16,13 @@ public class TaskManagerUtility {
         relation.setTaskGroup(taskGroup);
         relation.setTask(task);
         relation.setParentTaskId(parentTaskId);
+        if (task.getRelations() == null ) {
+            task.setRelations(new ArrayList<>());
+        }
         task.getRelations().add(relation);
+        if (taskGroup.getRelations() == null ) {
+            taskGroup.setRelations(new ArrayList<>());
+        }
         taskGroup.getRelations().add(relation);
         return taskGroup;
     }
