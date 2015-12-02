9d0fb45982ed64abad6f3c4499aa22ef275022d4
Divya Rai <divya.rai@flipkart.com>
2015-12-02 15:55:31 +0530

    added implementedby for subjectDao

--- src/main/java/com/tasks/manager/db/dao/interfaces/SubjectDao.java
+++ src/main/java/com/tasks/manager/db/dao/interfaces/SubjectDao.java
@@ -1,11 +1,14 @@
 package com.tasks.manager.db.dao.interfaces;
 
+import com.google.inject.ImplementedBy;
+import com.tasks.manager.db.dao.jpa.SubjectDaoImpl;
 import com.tasks.manager.db.model.entities.Relation;
 import com.tasks.manager.db.model.entities.Subject;
 
 /**
  * Created by shlok.chaurasia on 19/11/15.
  */
+@ImplementedBy(SubjectDaoImpl.class)
 public interface SubjectDao extends BaseDao<Subject>{
 
 }
