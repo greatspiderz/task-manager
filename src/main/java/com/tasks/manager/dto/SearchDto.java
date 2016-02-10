4ecf15a9d2b90ceaddf8f17a1c41e03b8e0f5d14
Akshay Kumar <akshay.kesarwan@flipkart.com>
2016-02-11 01:51:14 +0530

    Adding json ignore

--- src/main/java/com/tasks/manager/dto/SearchDto.java
+++ src/main/java/com/tasks/manager/dto/SearchDto.java
@@ -1,5 +1,7 @@
 package com.tasks.manager.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.tasks.manager.db.model.entities.Actor;
 import com.tasks.manager.db.model.entities.Subject;
 import com.tasks.manager.db.model.entities.TaskAttributes;
@@ -15,25 +17,47 @@ import java.util.List;
  * Created by divya.rai on 05/11/15.
  */
 @Data
-@JsonSnakeCase
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SearchDto {
 
+    @JsonProperty(value = "statuses")
     private List<TaskStatus> statuses = new ArrayList<>();
+
+    @JsonProperty(value = "from_date")
     private DateTime fromDate;
+
+    @JsonProperty(value = "to_date")
     private DateTime toDate;
 
     //TODO: To be deprecated later
+    @JsonProperty(value = "status")
     private TaskStatus status;
+
+    @JsonProperty(value = "created_at")
     private DateTime createdAt;
 
-
+    @JsonProperty(value = "type")
     private String type;
+
+    @JsonProperty(value = "level")
     private Integer level;
+
+    @JsonProperty(value = "actors")
     private List<Actor> actors;
+
+    @JsonProperty(value = "subject")
     private Subject subject;
+
+    @JsonProperty(value = "tenant")
     private String tenant;
+
+    @JsonProperty(value = "first_result")
     private Integer firstResult;
+
+    @JsonProperty(value = "max_results")
     private Integer maxResults;
+
+    @JsonProperty(value = "task_attributes")
     private TaskAttributes taskAttributes;
 
 }
